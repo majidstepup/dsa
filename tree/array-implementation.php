323235d0d92ec58314d0cd59b3a0b5004929d969
Md. Abdul Majid <abdulmajid.stepup@gmail.com>
2025-08-07 17:40:40 +0600

    pre order, in order, and post order traversal using array tree

--- tree/array-implementation.php
+++ tree/array-implementation.php
@@ -22,9 +22,53 @@ function getData(int $index): ?string
     return null;
 }
 
+function preOrder(int $index): string
+{
+    global $binaryTreeArray;
+
+    $result = '';
+    if (!($index >= count($binaryTreeArray) || !$binaryTreeArray[$index])) {
+        $result .= $binaryTreeArray[$index] . ', ';
+        $result .= preOrder(leftChildIndex($index));
+        $result .= preOrder(rightChildIndex($index));
+    }
+
+    return $result;
+}
+
+function inOrder(int $index): string
+{
+    global $binaryTreeArray;
+
+    $result = '';
+    if (!($index >= count($binaryTreeArray) || !$binaryTreeArray[$index])) {
+        $result .= inOrder(leftChildIndex($index));
+        $result .= $binaryTreeArray[$index] . ', ';
+        $result .= inOrder(rightChildIndex($index));
+    }
+
+    return $result;
+}
+
+function postOrder(int $index): string
+{
+    global $binaryTreeArray;
+
+    $result = '';
+    if (!($index >= count($binaryTreeArray) || !$binaryTreeArray[$index])) {
+        $result .= postOrder(leftChildIndex($index));
+        $result .= postOrder(rightChildIndex($index));
+        $result .= $binaryTreeArray[$index] . ', ';
+    }
+
+    return $result;
+}
+
 $rightChild = rightChildIndex(0);
 $leftChildOfRightChild = leftChildIndex($rightChild);
 $data = getData($leftChildOfRightChild);
 
-echo "root.right.left.data: $data";
-
+echo "root.right.left.data: $data\n";
+echo 'Pre-order Traversal: ' . rtrim(preOrder(0), ', ') . "\n";
+echo 'In-order Traversal: ' . rtrim(inOrder(0), ', ') . "\n";
+echo 'Post-order Traversal: ' . rtrim(postOrder(0), ', ') . "\n";
